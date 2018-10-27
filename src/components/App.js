a8ee01e330ca32a70131e78543cfca85dfd1ea5b
Ari Höysniemi <ari.hoysniemi@sysart.fi>
2018-10-28 00:17:04 +0300

    Removed settings.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Dashboard from 'components/dashboard/Dashboard';
-import Settings from 'components/settings/Settings';
 import Header from 'components/header/Header';
 import { Route } from 'react-router-dom';
 
@@ -10,7 +9,6 @@ export default class App extends React.Component {
       <div onClick={() => { console.log('hah') }}>
         <Header />
         <Route exact path="/" component={Dashboard} />
-        <Route path="/settings" component={Settings} />
       </div>
     );
   }
