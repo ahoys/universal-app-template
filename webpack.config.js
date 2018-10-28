446a225635aa956b1707a93c8296a455d19c72c0
Ari Höysniemi <ari.hoysniemi@gmail.com>
2018-10-28 18:55:36 +0200

    Separate config for the client.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-// Production config (default).
-// This will be automatically enabled. You can override settings in the dev-config below.
-const config = {
+// Production config for the server.
+// You can override settings in the dev-config below.
+const serverConfig = {
   mode: 'production',
   target: 'node',
-  entry: {
-    client: './src/client.js',
-    server: './src/server.js'
-  },
+  entry: './src/server.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: 'server.bundle.js'
   },
   module: {
     rules: [
@@ -36,15 +33,45 @@ const config = {
   },
 };
 
+// Production config for the client.
+// Client will have a different target.
+const clientConfig = {
+  mode: 'production',
+  target: 'node',
+  entry: './src/client.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'client.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.html$/, use: 'file-loader', exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src/'),
+      dist: path.resolve(__dirname, 'dist/'),
+      components: path.resolve(__dirname, 'src/components/'),
+      reducers: path.resolve(__dirname, 'src/reducers/'),
+      actions: path.resolve(__dirname, 'src/actions/'),
+    },
+    extensions: ['.js'],
+    modules: ['node_modules'],
+    plugins: [],
+  },
+};
+
 // Development overrides.
 // Use cross-env NODE_ENV = development to initialize.
 if (process.env.NODE_ENV === 'development') {
-  config.mode = 'development';
-  config.output.path = path.resolve(__dirname, 'dist/dev');
-  config.devtool = 'inline-source-map';
-  config.resolve.plugins = [
+  serverConfig.mode = 'development';
+  serverConfig.output.path = path.resolve(__dirname, 'dist/dev');
+  serverConfig.devtool = 'inline-source-map';
+  serverConfig.resolve.plugins = [
     new CleanWebpackPlugin(['dist/dev']),
   ];
 }
 
-module.exports = config;
+module.exports = [serverConfig, clientConfig];
