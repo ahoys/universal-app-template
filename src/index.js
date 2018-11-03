e4371d70178df5d776db2808b7167030539610f9
Ari Höysniemi <ari.hoysniemi@gmail.com>
2018-11-03 19:13:47 +0200

    Index should load dev config.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+/**
+ * Development index file.
+ * 
+ * The goal is to set up a hot module loading development server.
+ * To investigate build configuration for the development environment,
+ * see: ../webpack.development.js.
+ * 
+ * Production mode will skip this file entirely.
+ */
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
-const config = require('../webpack.config.js');
+const config = require('../webpack.development.js');
 const app = express();
 
 const compiler = webpack(config);
