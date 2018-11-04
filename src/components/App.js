06631d108cb9a184878044e3b30d5b1f8d3c093d
Ari Höysniemi <ari.hoysniemi@gmail.com>
2018-11-04 17:35:05 +0200

    Added settings example for Switch.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import Dashboard from 'components/dashboard/Dashboard';
 import Header from 'components/header/Header';
-import { Route } from 'react-router-dom';
+import Dashboard from 'components/dashboard/Dashboard';
+import Settings from 'components/settings/Settings';
+import { Switch, Route } from 'react-router';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
+        <Switch>
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/settings" component={Settings} />
+        </Switch>
       </div>
     );
   }
