316ecb44b8fae0131c7e098f5118bd9590cceef9
Ari Höysniemi <ari.hoysniemi@gmail.com>
2018-11-29 19:15:09 +0200

    Added login to nav for now.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,34 +17,27 @@ const UniversalSettings = universal(import('components/settings/Settings'));
 /**
  * The main React-application starts from here.
  */
-const App = ({ inSession }) => {
-  return inSession
-    ? (
-      <div className="App">
-        <Header />
-        <Content>
-          <Navigator>
-            <Link to="/">Home</Link>
-            <Link to="/dashboard">Go to dashboard</Link>
-            <Link to="/settings">Go to settings</Link>
-          </Navigator>
-          <View>
-            <Switch>
-              <Route exact path="/dashboard" component={UniversalDashboard} />
-              <Route exact path="/settings" component={UniversalSettings} />
-            </Switch>
-          </View>
-        </Content>
-        <Footer />
-      </div>
-    )
-    : (
-      <div className="App">
-        <UniversalDashboard />
-        <UniversalLogin />
-      </div>
-    );
-};
+const App = ({ inSession }) => (
+  <div className="App">
+    <Header />
+    <Content>
+      <Navigator>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Go to dashboard</Link>
+        <Link to="/settings">Go to settings</Link>
+        { inSession ? null : <Link to="/login">Login</Link> }
+      </Navigator>
+      <View>
+        <Switch>
+          <Route exact path="/dashboard" component={UniversalDashboard} />
+          <Route exact path="/settings" component={UniversalSettings} />
+          <Route exact path="/login" component={UniversalLogin} />
+        </Switch>
+      </View>
+    </Content>
+    <Footer />
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   inSession: state.getIn(['session', 'inSession']),
