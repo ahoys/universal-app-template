adcd94c6d1f410f7d99629beada0d2893cbd1048
Ari Höysniemi <ari.hoysniemi@gmail.com>
2018-12-01 12:19:47 +0200

    Strongly typed actions.session.

--- src/actions/actions.session.js
+++ src/actions/actions.session.js
@@ -1,10 +1,24 @@
 // @flow
 
-type Action = {
-  +username: string,
-  +password: string,
-  +token: string,
+type RequestSessionAction = {
+  +type: 'REQUEST_SESSION',
+  +payload: {
+    +username: string,
+    +password: string,
+  },
 };
+type ReceiveSessionAction = {
+  +type: 'RECEIVE_SESSION',
+  +payload: {
+    +username: string,
+    +token: string,
+    +inSession: boolean,
+  },
+};
+
+type Action =
+  | RequestSessionAction
+  | ReceiveSessionAction;
 
 /**
  * Requests a new session.
@@ -12,7 +26,7 @@ type Action = {
  * @param {string} password
  * @param {string} token
  */
-export const requestSession = ( username: Action, password: Action ) => ({
+export const requestSession = ( username: string, password: string ): RequestSessionAction => ({
   type: 'REQUEST_SESSION',
   payload: {
     username,
@@ -28,11 +42,11 @@ export const requestSession = ( username: Action, password: Action ) => ({
  * @param {string} username
  * @param {string} token
  */
-export const receiveSession = ( username: Action, token: Action ) => ({
+export const receiveSession = ( username: string, token: string ): ReceiveSessionAction => ({
   type: 'RECEIVE_SESSION',
   payload: {
     username,
     token,
-    inSession: username && token,
+    inSession: !!username && !!token,
   },
 });
